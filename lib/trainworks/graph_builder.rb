fc9965ffdb26e8e428005139c293cccd8b4dae8e
Andre Herculano <aherculano@babbel.com>
2016-11-29 21:38:52 +0100

    refactor trainworks/graphbuilder

--- lib/trainworks/graph_builder.rb
+++ lib/trainworks/graph_builder.rb
@@ -4,16 +4,26 @@ module Trainworks
       # TODO: refactor tuple to be a Route struct or something similar
       tuples.reduce({}) do |graph, tuple|
         from, to, distance = tuple
-        raise UnknwonRouteFormat if [from, to, distance].any?(&:nil?)
-        if graph.has_key?(from)
-          graph[from][to] = distance
-        else
-          graph[from] = Hash[to, distance]
-        end
-        graph
+        assert_route_is_valid([from, to, distance])
+        add_edge(graph, from, to, distance)
       end
     end
 
+    def self.add_edge(graph, from, to, distance)
+      if graph.has_key?(from)
+        graph[from][to] = distance
+      else
+        graph[from] = Hash[to, distance]
+      end
+      graph
+    end
+
     class UnknwonRouteFormat < ArgumentError; end
+
+    private
+
+    def self.assert_route_is_valid(route)
+      raise UnknwonRouteFormat if route.any?(&:nil?)
+    end
   end
 end
--- spec/trainworks/graph_builder_spec.rb
+++ spec/trainworks/graph_builder_spec.rb
@@ -3,6 +3,29 @@ require 'spec_helper'
 describe Trainworks::GraphBuilder do
   let(:builder) { Trainworks::GraphBuilder }
 
+  describe '.add_edge' do
+    it 'inserts a new node to the graph if it is not present' do
+      graph = {}
+      expect(builder.add_edge(graph, 'A', 'B', 10)).to eq(
+        'A' => { 'B' => 10 }
+      )
+    end
+
+    it 'inserts a new edge if the node already exists' do
+      graph = { 'A' => { 'B' => 10 } }
+      expect(builder.add_edge(graph, 'A', 'C', 20)).to eq(
+        'A' => { 'B' => 10, 'C' => 20 }
+      )
+    end
+
+    it 'overrides an edge if already exists' do
+      graph = { 'A' => { 'B' => 10 } }
+      expect(builder.add_edge(graph, 'A', 'B', 20)).to eq(
+        'A' => { 'B' => 20 }
+      )
+    end
+  end
+
   describe '.build' do
     context 'when the input is empty' do
       let(:input) { [] }
