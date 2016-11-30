952281ccf6cbb71b233d6ec9b749d01379e59917
Andre Herculano <aherculano@babbel.com>
2016-11-30 20:04:30 +0100

    add top level doc for GraphAlgorithm

--- lib/trainworks/graph_algorithm.rb
+++ lib/trainworks/graph_algorithm.rb
@@ -1,4 +1,16 @@
 module Trainworks
+  # GraphAlgorithm is used to calculate direct distances,
+  # shortest path and other paths between nodes.
+  # It assumes graph is a hash of adjacencies of a directed-weighted-possibly-cyclic-graph
+  # @example
+  # {
+  #   'A' => { 'B' => 5 },
+  #   'B' => { 'C' => 5 },
+  #   'C' => { 'B' => 4 }
+  # }
+  # In this graph, the node 'A' is connected to 'B' and have a distance of 5. The node
+  # 'B' is connected to 'C' with distance 5 as well. And finally, the node 'C' has a
+  # connection to 'B' with distance 4.
   class GraphAlgorithm
     def initialize(graph)
       @graph = graph
@@ -18,6 +30,7 @@ module Trainworks
       distance
     end
 
+    # NoSuchRoute is raised when there are no direct connection between two cities (nodes)
     class NoSuchRoute < KeyError
       def to_s
         'NO SUCH ROUTE'
