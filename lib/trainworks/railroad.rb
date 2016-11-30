20fb7a123bd7c95f8779b3c24a36dab72a88ead9
Andre Herculano <aherculano@babbel.com>
2016-11-30 20:54:16 +0100

    add top level doc to Railroad

--- lib/trainworks/railroad.rb
+++ lib/trainworks/railroad.rb
@@ -1,4 +1,9 @@
 module Trainworks
+  # Railroad is the entry point for Trainworks gem
+  # it accepts the path of the input file and optionally
+  # a class to parse this file,
+  # a class to build the graph
+  # and a class that will implement the graph related algorithms
   class Railroad
     def initialize(file_path, file_parser: FileParser, graph_builder: GraphBuilder, graph_algorithm: GraphAlgorithm)
       @file_parser = file_parser.new(file_path)
@@ -6,6 +11,8 @@ module Trainworks
       @algorithm = graph_algorithm.new(@graph)
     end
 
+    # @param [String] a string representing the route to be followed. E.g. A-B-C
+    # @return [Number] the distance from A to B to C
     def distance(route_string)
       @algorithm.distance(route_string.split('-'))
     end
