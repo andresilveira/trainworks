4f24923935ecfb290900df1712bdb8f1d9daff55
Andre Herculano <aherculano@babbel.com>
2016-11-30 12:38:17 +0100

    improve specs for Trainworks::Route

--- lib/trainworks.rb
+++ lib/trainworks.rb
@@ -1,6 +1,7 @@
 require 'trainworks/version'
 require 'trainworks/railroad'
 require 'trainworks/file_parser'
+require 'trainworks/route.rb'
 require 'trainworks/graph_builder'
 
 # TODO: add documentation
--- spec/trainworks/route_spec.rb
+++ spec/trainworks/route_spec.rb
@@ -20,4 +20,26 @@ describe Trainworks::Route do
   it 'responds to "distance"' do
     expect(route.distance).to eq(5.6)
   end
+
+  context 'should convert the parameters' do
+    let(:route) do
+      Trainworks::Route.new(
+        from: 15,
+        to:   :B,
+        distance: "1.6"
+      )
+    end
+
+    it '"from" is converted to string' do
+      expect(route.from).to eq("15")
+    end
+
+    it '"to" is converted to string' do
+      expect(route.to).to eq("B")
+    end
+
+    it '"distance" is converted to float' do
+      expect(route.distance).to eq(1.6)
+    end
+  end
 end
