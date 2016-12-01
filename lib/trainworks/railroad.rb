8c92198b9ae3f0b8357642f8cb1a0b7703bf92f2
Andre Herculano <aherculano@babbel.com>
2016-12-01 20:08:43 +0100

    call trips_with_max_distance in Railroad

--- lib/trainworks/railroad.rb
+++ lib/trainworks/railroad.rb
@@ -22,11 +22,13 @@ module Trainworks
     # @param [String] with_max_stops represents how many stops at most, the algorithm is allowed to 'walk'
     # in any direction starting from 'from'
     # @return [Array] a array of arrays which represents each path found
-    def trips(from:, to:, with_max_stops: nil, with_exact_stops: nil)
+    def trips(from:, to:, with_max_stops: nil, with_exact_stops: nil, with_max_distance: nil)
       if with_max_stops
         @algorithm.trips_with_max_stops(from: from, to: to, stops: with_max_stops)
       elsif with_exact_stops
         @algorithm.trips_with_exact_stops(from: from, to: to, stops: with_exact_stops)
+      elsif with_max_distance
+        @algorithm.trips_with_max_distance(from: from, to: to, distance: with_max_distance)
       else
         raise "I don't know how to calculate these routes"
       end
